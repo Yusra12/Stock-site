f52ef2a458c532ae22d4905f193c7072b9aeed46
Yusra Noor <yusra.noor@strathmore.edu>
2022-03-21 14:25:48 +0300

    Front-end UI

---
+++ stock-site/src/App.js
@@ -0,0 +1,17 @@
+
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle/>
+    <Navbar/>
+    <Hero/>
+    </div>
+  );
+}
+
+export default App;
